54f1942bbb1f4b8808931e9d9aa5afc7dff32815
Phan Trung Thuận <thuantk000@gmail.com>
2024-04-09 20:56:15 +0700

    Modify some css of topnav

--- topnav/topnav.js
+++ topnav/topnav.js
@@ -2,6 +2,13 @@ window.onload = function () {
     addEnrollmentClasses();
 }
 
+// Set position of my class dropdown content
+let dropdown_content_class = document.getElementById("dropdown-content-class");
+let rect = document.getElementById("my-class").getBoundingClientRect();
+dropdown_content_class.style.top = (rect.bottom + 1).toString() + 'px';
+dropdown_content_class.style.left = (rect.left + 13).toString() + 'px';
+console.log(rect);
+
 document.getElementById("logout-button").addEventListener("click", function() {
     const confirm = window.confirm("Are you sure?");
     if (confirm) {
@@ -15,7 +22,7 @@ async function addEnrollmentClasses() {
     let response = await sendGetClassRequest();
     let data = JSON.parse(response); 
     
-    console.log(data);
+    // console.log(data);
 
     let dropdown = document.getElementById("dropdown-content-class");
     for (let enroll_class of data) {
