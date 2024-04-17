4344efb9cf32c6d1d7fb39974569b7bf3f1a83b6
Phan Trung Thuận <thuantk000@gmail.com>
2024-04-17 12:29:15 +0700

    remove more cookie when logout

--- utils/functions.php
+++ utils/functions.php
@@ -46,6 +46,8 @@
             }
 
             if ($data['do'] === 'logout') {
+                setcookie('username', '', time() - 100, '/');
+                setcookie('password', '', time() - 100,'/');
                 setcookie('type', '', time() - 100, '/');
             }
 
